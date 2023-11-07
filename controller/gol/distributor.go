8184c3de8dd9c265cf3aa16bdc3dd8d710a51824
emirzaki <emirabouzaki@gmail.com>
2023-11-07 17:38:02 +0000

    put gol logic in node

--- controller/gol/distributor.go
+++ controller/gol/distributor.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"net/rpc"
-	"uk.ac.bris.cs/gameoflife/util"
 )
 
 type distributorChannels struct {
@@ -32,48 +31,13 @@ type Request struct {
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
+	// TODO: Create a 2D slice to store the world.
+
 	turn := 0
-	// send input command
-	c.ioCommand <- ioInput
-
-	//wait for idle
-	c.ioCommand <- ioCheckIdle
-	<-c.ioIdle
-
-	fmt.Println("running")
-
-	// initalize world
-	world := make([][]byte, p.ImageHeight)
-	for i := range world {
-		world[i] = make([]byte, p.ImageWidth)
-	}
-
-	// store image bytes into world
-	for y := 0; y < p.ImageHeight; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-			world[y][x] = <-c.ioInput
-		}
-	}
 
 	// TODO: Execute all turns of the Game of Life.
-	for ; turn < p.Turns; turn++ {
-		world = calculateNextState(p, world)
-	}
 
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
-	// put each byte of final wrold into output channel
-	for y := 0; y < p.ImageHeight; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-			c.ioOutput <- world[y][x]
-		}
-	}
-
-	// put ioOuput into command channel
-	c.ioCommand <- ioOutput
-
-	// finalturncompleteevent
-	c.events <- FinalTurnComplete{CompletedTurns: turn, Alive: calculateAliveCells(p, world)}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
@@ -83,73 +47,6 @@ func distributor(p Params, c distributorChannels) {
 
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
-}
-
-func getNumOfLiveNeighbours(world [][]byte, i int, j int) int {
-	numOfLiveNeighbours := 0
-
-	// positive modulus
-	up := ((i-1)%len(world) + len(world)) % len(world)
-	down := ((i+1)%len(world) + len(world)) % len(world)
-	right := ((j+1)%len(world[i]) + len(world[i])) % len(world[i])
-	left := ((j-1)%len(world[i]) + len(world[i])) % len(world[i])
-
-	neighbours := [8]byte{world[up][j], world[down][j], world[i][left], world[i][right], world[up][left], world[up][right], world[down][right], world[down][left]}
-
-	for _, neighbour := range neighbours {
-		if neighbour == ALIVE {
-			numOfLiveNeighbours++
-		}
-	}
-
-	return numOfLiveNeighbours
-
-}
-
-func calculateNextState(p Params, world [][]byte) [][]byte {
-	newWorld := make([][]byte, p.ImageHeight)
-
-	for i := 0; i < len(newWorld); i++ {
-		newWorld[i] = make([]byte, p.ImageWidth)
-	}
-
-	for i := 0; i < len(newWorld); i++ {
-		for j := 0; j < len(newWorld[i]); j++ {
-			// get number of live neighbours
-			numOfLiveNeighbours := getNumOfLiveNeighbours(world, i, j)
-
-			// rules of the game of life
-			if world[i][j] == ALIVE {
-				if numOfLiveNeighbours < 2 || numOfLiveNeighbours > 3 {
-					newWorld[i][j] = DEAD
-				} else {
-					newWorld[i][j] = ALIVE
-				}
-			} else {
-				if numOfLiveNeighbours == 3 {
-					newWorld[i][j] = ALIVE
-				}
-			}
-		}
-	}
-
-	return newWorld
-
-}
-
-func calculateAliveCells(p Params, world [][]byte) []util.Cell {
-	var aliveCells []util.Cell
-
-	for y := 0; y < len(world); y++ {
-		for x := 0; x < len(world[y]); x++ {
-			if world[y][x] == ALIVE {
-				// add cell coordinates to aliveCells slice
-				aliveCells = append(aliveCells, util.Cell{X: x, Y: y})
-			}
-		}
-	}
-
-	return aliveCells
 }
 
 func handleConnections() {
--- node/gol/distributor.go
+++ node/gol/distributor.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/rpc"
 	"time"
+	"uk.ac.bris.cs/gameoflife/util"
 )
 
 type distributorChannels struct {
@@ -16,6 +17,9 @@ type distributorChannels struct {
 	ioOutput   chan<- uint8
 	ioInput    <-chan uint8
 }
+
+const ALIVE byte = 255
+const DEAD byte = 0
 
 func (s *ControllerOperations) EvolveGoL(req Request, res *Response) (err error) {
 	res.Message = s.EvolveGoL(req.Message, "omak")
@@ -35,14 +39,44 @@ type ControllerOperations struct{}
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
-	pAddr := flag.String("port", "8030", "Port to listen on")
-	flag.Parse()
-	rand.Seed(time.Now().UnixNano())
-	rpc.Register(&ControllerOperations{})
-	ln, _ := net.Listen("tcp", ":"+*pAddr)
-	defer ln.Close()
-	rpc.Accept(ln)
 	turn := 0
+
+	// Make sure that the Io has finished any output before exiting.
+	c.ioCommand <- ioCheckIdle
+	<-c.ioIdle
+
+	// initalize world
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.ImageWidth)
+	}
+
+	// store image bytes into world
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			world[y][x] = <-c.ioInput
+		}
+	}
+
+	// TODO: Execute all turns of the Game of Life.
+	for ; turn < p.Turns; turn++ {
+		world = calculateNextState(p, world)
+	}
+
+	// TODO: Report the final state using FinalTurnCompleteEvent.
+
+	// put each byte of final wrold into output channel
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			c.ioOutput <- world[y][x]
+		}
+	}
+
+	// put ioOuput into command channel
+	c.ioCommand <- ioOutput
+
+	// finalturncompleteevent
+	c.events <- FinalTurnComplete{CompletedTurns: turn, Alive: calculateAliveCells(p, world)}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
@@ -52,4 +86,81 @@ func distributor(p Params, c distributorChannels) {
 
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
+}
+
+func getNumOfLiveNeighbours(world [][]byte, i int, j int) int {
+	numOfLiveNeighbours := 0
+
+	// positive modulus
+	up := ((i-1)%len(world) + len(world)) % len(world)
+	down := ((i+1)%len(world) + len(world)) % len(world)
+	right := ((j+1)%len(world[i]) + len(world[i])) % len(world[i])
+	left := ((j-1)%len(world[i]) + len(world[i])) % len(world[i])
+
+	neighbours := [8]byte{world[up][j], world[down][j], world[i][left], world[i][right], world[up][left], world[up][right], world[down][right], world[down][left]}
+
+	for _, neighbour := range neighbours {
+		if neighbour == ALIVE {
+			numOfLiveNeighbours++
+		}
+	}
+
+	return numOfLiveNeighbours
+
+}
+
+func calculateNextState(p Params, world [][]byte) [][]byte {
+	newWorld := make([][]byte, p.ImageHeight)
+
+	for i := 0; i < len(newWorld); i++ {
+		newWorld[i] = make([]byte, p.ImageWidth)
+	}
+
+	for i := 0; i < len(newWorld); i++ {
+		for j := 0; j < len(newWorld[i]); j++ {
+			// get number of live neighbours
+			numOfLiveNeighbours := getNumOfLiveNeighbours(world, i, j)
+
+			// rules of the game of life
+			if world[i][j] == ALIVE {
+				if numOfLiveNeighbours < 2 || numOfLiveNeighbours > 3 {
+					newWorld[i][j] = DEAD
+				} else {
+					newWorld[i][j] = ALIVE
+				}
+			} else {
+				if numOfLiveNeighbours == 3 {
+					newWorld[i][j] = ALIVE
+				}
+			}
+		}
+	}
+
+	return newWorld
+
+}
+
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+	var aliveCells []util.Cell
+
+	for y := 0; y < len(world); y++ {
+		for x := 0; x < len(world[y]); x++ {
+			if world[y][x] == ALIVE {
+				// add cell coordinates to aliveCells slice
+				aliveCells = append(aliveCells, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+
+	return aliveCells
+}
+
+func handleConnections() {
+	pAddr := flag.String("port", "8030", "Port to listen on")
+	flag.Parse()
+	rand.Seed(time.Now().UnixNano())
+	rpc.Register(&ControllerOperations{})
+	ln, _ := net.Listen("tcp", ":"+*pAddr)
+	defer ln.Close()
+	rpc.Accept(ln)
 }
